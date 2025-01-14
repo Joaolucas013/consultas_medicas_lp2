849c3ca89f5d999e879d3c1a80510a332fb0bdbb
João Lucas <164104520+Joaolucas013@users.noreply.github.com>
2025-01-14 17:40:47 -0300

    up

--- src/main/java/org/example/paciente/PacienteDto.java
+++ src/main/java/org/example/paciente/PacienteDto.java
@@ -1,8 +1,24 @@
 package org.example.paciente;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 import java.time.LocalDateTime;
 
-public record PacienteDto(String nome, String sexo, int idade, LocalDateTime consulta){
+public record PacienteDto(
+        @NotBlank
+        String nome,
+        @NotBlank
+        String sexo,
+
+        @NotNull
+        @Positive
+        int idade,
+        @NotNull
+        @Future
+        LocalDateTime consulta) {
 
     public PacienteDto(Paciente p){
         this(p.getNome(), p.getSexo(), p.getIdade(), p.getConsulta());
